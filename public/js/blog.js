a50f7393bd6a01c223ed670322799a65aa1ac8c2
Seven Souls <46326574+SevenSoulsss@users.noreply.github.com>
2022-01-20 17:41:02 +0100

    up

--- public/js/blog.js
+++ public/js/blog.js
@@ -27,6 +27,10 @@ function addComment() {
     p.innerText = message
     article.append(p)
 
+    const button = document.createElement('article')
+    button.className = "btn btn-primary"
+    button.innerText = 'Modifier'
+    article.append(button)
 
     document.getElementById('avis-list').append(article)
 }
@@ -54,37 +58,3 @@ document.getElementsByTagName('form')[1].addEventListener (
 
     }
 )
-
-
-function addArticle(){
-    
-    const all_comment = document.getElementsByTagName("article")
-
-    const article = document.createElement('article')
-    article.id = "comment-" + all_comment.length
-    article.className = "comment border rounded p-2 mb-3"
-
-    const header = document.createElement('header')
-    header.className = "mb-2 border-bottom"
-    article.prepend(header)
-
-    const strong = document.createElement('strong')
-    strong.className = "d-block"
-    const name = document.getElementById('nom').value
-    strong.innerText = name
-    header.prepend(strong)
-
-    const time = document.createElement('time')
-    date = getDate()
-    time.dateTime = date[0]
-    time.className = "d-block fst-italic"
-    time.innerText = date[1]
-    header.append(time)
-
-    const p = document.createElement('p')
-    const message = document.getElementsByTagName('textarea')[0].value
-    p.innerText = message
-    article.append(p)
-
-    document.getElementById('avis-list').append(article)
-}
