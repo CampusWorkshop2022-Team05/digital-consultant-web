d4194e37003ccf14261b4de78facea468d570d03
Seven Souls <46326574+SevenSoulsss@users.noreply.github.com>
2022-01-21 05:01:43 +0100

    patch

--- public/js/blog.js
+++ public/js/blog.js
@@ -1,60 +1,56 @@
-function addComment() {
-    const all_comment = document.getElementsByTagName("article")
+function addComment(name, message){
+    
+    const CommentEdit = document.getElementById("comment-0")
 
-    const article = document.createElement('article')
-    article.id = "comment-" + all_comment.length
-    article.className = "comment border rounded p-2 mb-3"
+    const Clone = CommentEdit.cloneNode(true)
 
-    const header = document.createElement('header')
-    header.className = "mb-2 border-bottom"
-    article.prepend(header)
+    const CommentList = document.getElementById("comments-list")
 
-    const strong = document.createElement('strong')
-    strong.className = "d-block"
-    const name = document.getElementById('nom').value
-    strong.innerText = name
-    header.prepend(strong)
+    const Cloned = CommentList.appendChild(Clone)
 
-    const time = document.createElement('time')
-    date = getDate()
-    time.dateTime = date[0]
-    time.className = "d-block fst-italic"
-    time.innerText = date[1]
-    header.append(time)
+    const Author = Cloned.getElementsByTagName("strong")[0];
 
-    const p = document.createElement('p')
-    const message = document.getElementsByTagName('textarea')[0].value
-    p.innerText = message
-    article.append(p)
+    Author.innerHTML = name
 
-    const button = document.createElement('article')
-    button.className = "btn btn-primary"
-    button.innerText = 'Modifier'
-    article.append(button)
+    const Time = Cloned.getElementsByTagName("time")[0];
 
-    document.getElementById('avis-list').append(article)
+    const Timestamp = new Date()
+
+    const options = { day: 'numeric', month: 'long', year: 'numeric', hourCycle: "h24", day: "numeric", hour: "2-digit", minute: "2-digit" };
+
+    Time.innerHTML = `le ${(Timestamp.toLocaleDateString("fr", options)).replace(/\, /g, " à ").replace(":", "h")}<button class="btn-class btn-prm" onclick="EditComment()">Edit</button>`
+
+    const Description = Cloned.getElementsByTagName("p");
+
+    Description[0].innerHTML = message
 }
 
-function getDate() {
-    const date = new Date(Date.now())
-    const allMonths = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"]
-    const annee = date.getFullYear()
-    const mois = date.getMonth()
-    const day = date.getDate()
-    const heure = date.getHours()
-    const minute = date.getMinutes()
-    const date_time = annee + "-" + (mois+1) + "-" + day + " " + heure + ":" + minute + ":" + date.getSeconds()
-    const inner_text = "le " +  day + " " + allMonths[mois] + " " + annee + " à " +  heure + "h" + minute
-    return [date_time, inner_text]
+function EditComment(){
+    
+    const InputButton = document.getElementsByTagName("input");
+
+    const Name = document.getElementById("nom");
+
+    Name.setAttribute("value", "")
+
+    const Content = document.getElementById("message-content");
+
+    Content.setAttribute("value", "")
+
+    InputButton[1].defaultValue = "Update"
+
+    console.log("ok")
 }
 
-document.getElementsByTagName('form')[1].addEventListener (
-    'submit',
-    function (event) {
-        event.preventDefault()
-        addComment()
-        document.getElementById('nom').value = ""
-        document.getElementsByTagName('textarea')[0].value = ""
+function onReceived(){ // event.preventDefault() pour empêcher le refresh plutôt que d'utiliser le return false
 
-    }
-)
+    const Name = document.getElementById("nom").value
+
+    const Content = document.getElementById("message-content").value
+
+    if(Name === "" || Content === "") return;
+
+    addComment(Name, Content)
+
+    document.getElementById("new-comment").reset()
+}
